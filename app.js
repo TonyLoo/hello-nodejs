6f93c9c252d9c3fb0b96c8f49951ceed4c762fb4
TonyLoo <lorelei_dante@yahoo.com>
2017-09-24 19:33:08 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// give us your http
+
+var http = require('http');
+
+ 
+
+// Make us a server with that http
+
+http.createServer(function(request, response) {
+
+
+// We're 200… OK 
+
+    response.writeHead(200);
+
+
+// Achievement unlocked!
+
+    response.write('Hello OSF interns!');
+
+// End mission
+
+    response.end();
+
+}).listen(80);
+
+// Let us know you are working
+
+console.log('Listening on port 8080…');
